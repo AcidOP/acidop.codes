e11da23e1d3eb2c36edff49c675969a6e1914e5c
AcidOP <zeeshanalivr46@gmail.com>
2022-10-07 15:46:34 +0530

    🐛 Fixes in MDX render

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,10 @@ import 'katex/dist/katex.css'
 
 import '@fontsource/inter/variable-full.css'
 
+import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 
-import ScrollTop from '@/components/ScrollTop'
+import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
@@ -16,18 +17,15 @@ const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
-      <div>
-        {isDevelopment && isSocket && <ClientReload />}
-        <Analytics />
-        <LayoutWrapper>
-          <ScrollTop />
-          <Component {...pageProps} />
-        </LayoutWrapper>
-      </div>
-    </>
+      {isDevelopment && isSocket && <ClientReload />}
+      <Analytics />
+      <LayoutWrapper>
+        <Component {...pageProps} />
+      </LayoutWrapper>
+    </ThemeProvider>
   )
 }
