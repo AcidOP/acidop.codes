6b44a40faad5d16e004b839b96e9092860c368ed
AcidOP <zeeshanalivr46@gmail.com>
2022-10-10 12:13:34 +0530

    🚀 Revert to old Navbar without emoji

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,15 +1,15 @@
 const headerNavLinks = [
-  { href: '/', title: 'Home 🏠' },
-  { href: '/about', title: 'About 🤵🏻' },
-  { href: '/blog', title: 'Blogs 📚' },
-  { href: '/projects', title: 'Projects 🔨' },
-  { href: '/contact', title: 'Contact 🤳🏻' },
+  { href: '/', title: 'Home' },
+  { href: '/about', title: 'About' },
+  { href: '/blog', title: 'Blogs' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/contact', title: 'Contact' },
   {
     type: 'dropdown',
-    title: 'Other 👀',
+    title: 'Other',
     links: [
-      { href: '/tags', title: 'Tags 🔖' },
-      { href: '/tools', title: 'Uses ⚙' },
+      { href: '/tags', title: 'Tags' },
+      { href: '/tools', title: 'Uses' },
     ],
   },
 ]
