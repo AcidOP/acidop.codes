b9f72fe9d9a03b443e9d31a361d5150daa5b6987
AcidOP <zeeshanalivr46@gmail.com>
2023-03-25 22:53:54 +0530

    🐛 Prettier Error

--- components/Title.js
+++ components/Title.js
@@ -4,7 +4,7 @@ const Title = ({ children }) => {
   return (
     <>
       <br />
-      <h1 className="mb-4 text-primary-500 text-3xl font-bold tracking-tight md:text-4xl lg:text-5xl">{children}</h1>
+      <h1 className="mb-4 text-3xl font-bold tracking-tight md:text-4xl lg:text-5xl">{children}</h1>
     </>
   )
 }
